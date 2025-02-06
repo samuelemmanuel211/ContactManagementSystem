b731f39f380e451e09e5dfd1ad40035948bfd016
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-06 13:30:15 +0300

    Instance of Variables
    On line 8 to 28 instance of variables were added.

--- contactmanager.ui/src/main/java/contactmanager/ui/ContactManager.java
+++ contactmanager.ui/src/main/java/contactmanager/ui/ContactManager.java
@@ -1,6 +1,29 @@
 package contactmanager.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class ContactManager extends JFrame {
+
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private JPanel contactListPanel;
+    private JPanel contactDetailPanel;
+    private JPanel contactFormPanel;
+
+    private ArrayList<Contact> contacts;
+
+    private int selectedContactIndex = -1;
+
+    private JTextField formNameField;
+    private JTextField formPhoneField;
+    private JTextField formEmailField;
+
+    private JLabel detailNameLabel;
+    private JLabel detailPhoneLabel;
+    private JLabel detailEmailLabel;
+
+    
 }
